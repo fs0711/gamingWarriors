a40a10f454e0885b21c2acc99adcef19daf4f287
Abdul Hadi <aabdul.hadi@outlook.com>
2024-08-31 16:59:09 +0500

    branch management fix

--- gwBackend/BranchManagement/models/Branch.py
+++ gwBackend/BranchManagement/models/Branch.py
@@ -5,6 +5,7 @@
 # Local imports
 from gwBackend.generic import models
 from gwBackend.generic import db
+from gwBackend.UserManagement.models import User
 from gwBackend.generic.services.utils import constants, common_utils
 
 class Branch(models.Model):
@@ -18,9 +19,7 @@ class Branch(models.Model):
             constants.BRANCH__GAME_TYPES: [{"rule": "datatype", "datatype": list},
                                            {"rule": "collection_format", "datatype": list,
                                            "validation_rules": [{"rule": "required"}]}],        
-            constants.BRANCH__USERS: [{"rule": "datatype", "datatype": list},
-                                           {"rule": "collection_format", "datatype": list,
-                                            "validation_rules": [{"rule": "required"}]}],
+            constants.BRANCH__USERS: [{"rule": "datatype", "datatype": str},{"rule": "required"}],
             constants.BRANCH__OPENING_TIME: [{"rule": "required"}],
             constants.BRANCH__CLOSING_TIME: [{"rule": "required"}],
         }
@@ -31,7 +30,6 @@ class Branch(models.Model):
             constants.BRANCH__CITY: [{"rule": "existent"}],
             constants.BRANCH__OPENING_TIME: [{"rule": "existent"}],
             constants.BRANCH__CLOSING_TIME: [{"rule": "existent"}],
-
     }
     
     
@@ -43,6 +41,7 @@ class Branch(models.Model):
     game_types = db.ListField()
     opening_time = db.IntField(required=True)
     closing_time = db.IntField(required=True)
+    users = db.LazyReferenceField("User")
     
     
     def __str__(self):
@@ -55,7 +54,7 @@ class Branch(models.Model):
             constants.BRANCH__NAME: self[constants.BRANCH__NAME],
             constants.BRANCH__CITY: self[constants.BRANCH__CITY],
             constants.BRANCH__GAME_TYPES: self[constants.BRANCH__GAME_TYPES],
-            constants.BRANCH__USERS: self[constants.BRANCH__USERS],
+            constants.BRANCH__USERS: self[constants.BRANCH__USERS].fetch().name,
             constants.BRANCH__OPENING_TIME: self[constants.BRANCH__OPENING_TIME],
             constants.BRANCH__CLOSING_TIME: self[constants.BRANCH__CLOSING_TIME],
             constants.STATUS: self[constants.STATUS],
