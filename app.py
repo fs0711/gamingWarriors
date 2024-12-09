6489ca2abb786fa7aece233345330956d00d0a39
Abdul Hadi <aabdul.hadi@outlook.com>
2024-12-10 02:27:02 +0500

    SSL Update

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from gwBackend import app
 def _run():
     """ Imports the app and runs it. """
 app.run(debug=True, host="0.0.0.0", port=5000)
+#app.run(debug=True, host="0.0.0.0", port=5000,ssl_context=('cert.pem', 'key.pem') )
+
 
 if __name__ == '__main__':
     _run()
