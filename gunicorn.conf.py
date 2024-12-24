edeef728f3750aab577d71a7d3be4973ddcf6360
fs0711 <cs_1014@hotmail.com>
2024-12-25 04:34:42 +0500

    .

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 bind = "unix:gwbackend.sock"
 workers = 1
-accesslog = "/var/log/gwbackend/reqlog"
-errorlog = "/var/log/gwbackend/errlog"
+accesslog = "/var/log/gwbackend/access.log"
+errorlog = "/var/log/gwbackend/error.log"
 umask = 7
