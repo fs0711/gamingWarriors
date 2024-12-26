44fbf52f3988bb0eda780c5bb832d3f087bb1774
fs0711 <cs_1014@hotmail.com>
2024-12-26 12:22:13 +0500

    .

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from gwBackend import app
 
 def _run():
     """ Imports the app and runs it. """
-app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 #app.run(debug=True, host="0.0.0.0", port=5000,ssl_context=('cert.pem', 'key.pem') )
 
 
