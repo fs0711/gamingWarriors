b48723798d23268773673b97342a327dc1ba791e
fs0711 <cs_1014@hotmail.com>
2025-01-01 19:47:43 +0500

    accounts controlelr update

--- gwBackend/AccountsManagement/controllers/AccountsController.py
+++ gwBackend/AccountsManagement/controllers/AccountsController.py
@@ -64,6 +64,7 @@ class AccountsController(Controller):
                 response_data=obj.display()
             )
 
+
     @classmethod
     def read_controller(cls, data):
                 return response_utils.get_response_object(
@@ -72,71 +73,3 @@ class AccountsController(Controller):
             response_data=[
                 obj.display() for obj in cls.db_read_records(read_filter=data)
             ])
-                
-    @classmethod
-    def read_UID_controller(cls, data):
-        obj = cls.db_read_single_record(read_filter={constants.RFCARD__UID:data})
-        if obj:    
-            return str(obj.id)
-        else:
-            return 0
-        
-    @classmethod
-    def get_transactions(cls):
-        return response_utils.get_json_response_object(
-        response_code=response_codes.CODE_SUCCESS,
-        response_message=response_codes.MESSAGE_SUCCESS,
-        response_data=[{'transaction_id':obj[constants.ACCOUNTS__ID],'amount':obj[constants.ACCOUNTS__AMOUNT],'purpose':obj[constants.ACCOUNTS__PURPOSE],'type':obj[constants.ACCOUNTS__TYPE],'branch':str(obj[constants.ACCOUNTS__BRANCH].fetch().name),'organization':str(obj[constants.ACCOUNTS__ORGANIZATION].fetch().name)  } for obj in cls.db_read_records(read_filter={})],
-        )
-
-# , 'member':str(obj[constants.ACCOUNTS__MEMBER_ID].fetch().member_id)
-    # @classmethod
-    # def update_controller(cls, data):
-    #     is_valid, error_messages, obj = cls.db_update_single_record(
-    #         read_filter={constants.ID: data[constants.ID]}, update_filter=data
-    #     )
-    #     if not is_valid:
-    #         return response_utils.get_response_object(
-    #             response_code=response_codes.CODE_VALIDATION_FAILED,
-    #             response_message=response_codes.MESSAGE_VALIDATION_FAILED,
-    #             response_data=error_messages
-    #         )
-    #     if not obj:
-    #         return response_utils.get_response_object(
-    #             response_code=response_codes.CODE_RECORD_NOT_FOUND,
-    #             response_message=response_codes.MESSAGE_NOT_FOUND_DATA.format(
-    #                 constants.CLIENT.title(), constants.ID
-    #             ))
-    #     return response_utils.get_response_object(
-    #         response_code=response_codes.CODE_SUCCESS,
-    #         response_message=response_codes.MESSAGE_SUCCESS,
-    #         response_data=obj.display(),
-    #     )
-
-    # @classmethod
-    # def suspend_controller(cls, data):
-    #     _, _, obj = cls.db_update_single_record(
-    #         read_filter={constants.ID: data[constants.ID]},
-    #         update_filter={
-    #             constants.STATUS: constants.OBJECT_STATUS_SUSPENDED},
-    #         update_mode=constants.UPDATE_MODE__PARTIAL,
-    #     )
-    #     if obj:
-    #         return response_utils.get_response_object(
-    #             response_code=response_codes.CODE_SUCCESS,
-    #             response_message=response_codes.MESSAGE_SUCCESS,
-    #             response_data=obj.display(),
-    #         )
-    #     return response_utils.get_response_object(
-    #         response_code=response_codes.CODE_RECORD_NOT_FOUND,
-    #         response_message=response_codes.MESSAGE_NOT_FOUND_DATA.format(
-    #             constants.GAMEUNIT.title(), constants.ID
-    #         ))
-        
-    # @classmethod
-    # def get_rfcards(cls,data):
-    #     return response_utils.get_json_response_object(
-    #     response_code=response_codes.CODE_SUCCESS,
-    #     response_message=response_codes.MESSAGE_SUCCESS,
-    #     response_data=[obj.display_min() for obj in cls.db_read_records(read_filter=data)],
-    #     )
